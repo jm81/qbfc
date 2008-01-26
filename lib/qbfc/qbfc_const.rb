17471352491946a54a1a6123f5a5a95c6163fb0d
Jared Morgan <jmorgan@morgancreative.net>
2008-01-26 16:00:39 +0000

    QBFC_CONST - add spec and update documentation.

--- lib/qbfc/qbfc_const.rb
+++ lib/qbfc/qbfc_const.rb
@@ -2,6 +2,11 @@
 # For example:
 # 
 #   QBFC_CONST::DmToday # => 1 (for DateMacro)
+#   
+# The constants defined in the SDK documents all begin with a lower case letter.
+# In contrast, WIN32OLE capitalizes the constants to follow Ruby naming conventions:
+# 
+#   QBFC_CONST::DmToday instead of QBFC_CONST::dmToday
 
 module QBFC_CONST
 end
