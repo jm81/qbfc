d036546b55d48d2075653770082278e8685fcb54
Jared Morgan <jmorgan@morgancreative.net>
2008-02-16 21:55:55 +0000

    Break base_specs up (goal is generally one file per method or closely related methods).

--- spec/unit/base/allows_spec.rb
+++ spec/unit/base/allows_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require File.dirname(__FILE__) + '/../../spec_helper'
 
 # define some classes that inherit from Base with different "ALLOWS_*" constants
 module QBFCSpec
@@ -23,18 +23,7 @@ module QBFCSpec
   end
 end
 
-describe QBFC::Base do
-
-  before(:each) do 
-    # @ole_object represents the @ole_object in the class descended from
-    # Base. Some specs will be checking that this is sent a 'qbfc_method_missing'
-    # message with a given set of arguments. For specs that translate those
-    # arguments correct, see OLEWrapper spec.
-    @sess = mock(QBFC::Session)
-    @ole_object = mock(QBFC::OLEWrapper)
-    @ole_object.should_receive(:kind_of?).with(QBFC::OLEWrapper).and_return(true)
-    @base = QBFC::Base.new(@sess, @ole_object)
-  end
+describe "QBFC::Base::allows_*" do
   
   it "should specify if it allows create operations" do
     QBFC::Base::allows_create?.should be_false
@@ -60,18 +49,5 @@ describe QBFC::Base do
     QBFC::Base::allows_void?.should be_false
     QBFCSpec::Void::allows_void?.should be_true
   end
-  
-  it "should alias txn_id as id for transaction" do
-    @ole_object.should_receive(:respond_to_ole?).with(:TxnID).and_return(true)
-    @ole_object.should_receive(:respond_to_ole?).with(:ListID).and_return(false)
-    @ole_object.should_receive(:txn_id).and_return('T123')
-    @base.id.should == 'T123'
-  end
-  
-  it "should alias list_id as id for list items" do
-    @ole_object.should_receive(:respond_to_ole?).with(:ListID).and_return(true)
-    @ole_object.should_receive(:list_id).and_return('L123')
-    @base.id.should == 'L123'
-  end  
   
 end
