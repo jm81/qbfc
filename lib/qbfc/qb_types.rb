648ba04c9f7391eceb22d32094ca799105a0639e
Jared Morgan <jmorgan@morgancreative.net>
2008-02-17 23:06:35 +0000

    qb_types - more organizational changes of types arrays.

--- lib/qbfc/qb_types.rb
+++ lib/qbfc/qb_types.rb
@@ -34,27 +34,31 @@ QBFC_NO_MOD_TYPES = %w{ARRefundCreditCard BillPaymentCreditCard BillingRate Cust
 # Types that allow Query and Delete only
 QBFC_DELETE_ONLY = %w{PayrollItemNonWage DataEventRecoveryInfo}
 
-# Report types support Query requests only and return an itemized list of some sort;
-# sometimes, this includes meta-information about the report (e.g. header)
-QBFC_REPORT_TYPES = %w{AgingReport BillToPay BudgetSummaryReport CustomDetailReport CustomSummaryReport
-                      GeneralDetailReport GeneralSummaryReport 
-                    JobReport ListDeleted PayrollDetailReport PayrollSummaryReport 
-                    ReceivePaymentToDeposit SalesTaxPaymentCheck Template TimeReport TxnDeleted}
+# Report types return an IReportRet
+QBFC_REPORT_TYPES = %w{AgingReport BudgetSummaryReport CustomDetailReport CustomSummaryReport
+                      GeneralDetailReport GeneralSummaryReport JobReport PayrollDetailReport PayrollSummaryReport TimeReport }
 
 # Info Types support Query requests only and return a single entry
-QBFC_INFO_TYPES = %w{Company CompanyActivity Host Preferences ItemAssembliesCanBuild}
+QBFC_INFO_TYPES = %w{Company CompanyActivity Host Preferences}
 
 # Types that allow Special adds (Pre-defined and normally added automatically by QuickBooks)
 QBFC_HAS_SPECIAL_ADD = %w{Account Item}
 
 # TODO: Here and below arrays I haven't yet formed any approach to dealing with.
 # I leave them here as a reminder.
-ELEMENTS_ADD_MOD = %w{ DataExt }                  
+ELEMENTS_ADD_MOD = %w{ DataExt }         
                     
-ELEMENTS_ADD_MOD_QUERY = %w{ DataExtDef }                    
+ELEMENTS_ADD_MOD_QUERY = %w{ DataExtDef }
                     
 # Types that have their own DelRq
 ELEMENT_DEL_TYPES = %w{DataEventRecoveryInfo DataExt DataExtDef}
+
+# Query types support Query requests only and return an itemized list of some sort;
+# most of these may be integrated as special finders for their types.
+QBFC_QUERY_TYPES =  %w{BillToPay ListDeleted ReceivePaymentToDeposit Template TxnDeleted SalesTaxPaymentCheck}
+
+QBFC_ANOTHER_TO_INTEGRATE_SOMEWHERE = %w{ ItemAssembliesCanBuild }
+
 
 module QBFC
   # Create QBElement classes
