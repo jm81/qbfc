2dcda808c068a1005ece359c264c32d62fc89930
Jared Morgan <jmorgan@morgancreative.net>
2008-02-19 19:41:54 +0000

    Add Transaction finders: - .find_by_id - .find_by_ref - .find_by_ref_or_id

--- lib/qbfc/transaction.rb
+++ lib/qbfc/transaction.rb
@@ -4,6 +4,33 @@ module QBFC
   class Transaction < Element
     is_base_class
     
+    class << self
+      
+      # Find by Reference Number of the Transaction record.
+      # +options+ are the same as those for in +find+.
+      def find_by_ref(sess, ref, options = {})
+        q = create_query(sess)
+        q.send('ORTxnQuery').RefNumberList.Add(ref)
+        find(sess, :first, q, options)
+      end
+      
+      # Find by TxnID of List record.
+      # +options+ are the same as those for in +find+.      
+      def find_by_id(sess, id, options = {})
+        q = create_query(sess)
+        q.send('ORTxnQuery').TxnIDList.Add(id)
+        find(sess, :first, q, options)
+      end
+      
+      # Find by either ref or id. Tries id first, then ref.
+      def find_by_ref_or_id(*args)
+        find_by_id(*args) || find_by_ref(*args)
+      end
+      
+      alias_method :find_by_unique_id, :find_by_ref_or_id
+      
+    end
+    
     # Alias of TxnID for this record.
     def id
       @ole.txn_id
--- spec/unit/transaction_finders_spec.rb
+++ spec/unit/transaction_finders_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 module QBFC::Test
   class TxnFind < QBFC::Transaction
-    def qb_name
+    def self.qb_name
       "Check"
     end
   end
@@ -14,6 +14,19 @@ describe QBFC::Transaction do
     @sess = mock(QBFC::Session)
     @ole_wrapper = mock(QBFC::OLEWrapper)
     @txn = QBFC::Test::Txn.new(@sess, @ole_wrapper)
+    
+    # Request related mocks
+    @request = mock("QBFC::Request")
+    @txn_query = mock("QBFC::OLEWrapper#txn_query")
+    @response = mock("QBFC::Request#response")
+  end
+  
+  def setup_request
+    QBFC::Request.should_receive(:new).with(@sess, 'CheckQuery').and_return(@request)
+    @request.should_receive(:kind_of?).with(QBFC::Request).and_return(true)
+    @request.should_receive(:response).and_return(@response)
+    @response.stub!(:GetAt).with(0).and_return(@ole_wrapper)
+    @response.stub!(:ole_methods).and_return(["GetAt"])
   end
 
   describe ".find" do
@@ -29,26 +42,95 @@ describe QBFC::Transaction do
     it "passes additional arguments to Request"
     it "should get request#response"
     
-    describe ".find(for base_class Lists)" do 
+    describe ".find(for base_class Txns)" do 
       it "should request only TxnID"
-      it "should send class ChildList::find_by_id with TxnID and find options for each"
+      it "should send class ChildTxn::find_by_id with TxnID and find options for each"
     end  
   end
   
   describe ".find_by_ref" do
-    it "should set up Request, specifying RefNumberList"
-    it "should process Request"
+    before(:each) do 
+      @ref_list = mock("QBFC::OLEWrapper#ref_list")
+    end
+    
+    def setup_request
+      super
+      @request.should_receive(:ORTxnQuery).and_return(@txn_query)
+      @txn_query.should_receive(:RefNumberList).and_return(@ref_list)
+      @ref_list.should_receive(:Add).with("12345")
+    end
+    
+    it "should set up Request, specifying RefNumberList" do
+      setup_request
+      QBFC::Test::TxnFind.find_by_ref(@sess, "12345")
+    end
+  
+    it "should return a Transaction object" do
+      setup_request
+      list = QBFC::Test::TxnFind.find_by_ref(@sess, "12345")
+      list.should be_kind_of(QBFC::Test::TxnFind)
+    end
+  
+    it "should return nil if none found" do
+      setup_request
+      @response.should_receive(:GetAt).with(0).and_return(nil)
+      QBFC::Test::TxnFind.find_by_ref(@sess, "12345").should be_nil
+    end
   end
   
   describe ".find_by_id" do
-    it "should set up Request, specifying TxnIDList"
-    it "should process Request"
+    before(:each) do 
+      @txn_id_list = mock("QBFC::OLEWrapper#txn_id_list")
+    end
+    
+    def setup_request
+      super
+      @request.should_receive(:ORTxnQuery).and_return(@txn_query)
+      @txn_query.should_receive(:TxnIDList).and_return(@txn_id_list)
+      @txn_id_list.should_receive(:Add).with("123-456")
+    end
+    
+    it "should set up Request, specifying TxnIDTxn" do
+      setup_request
+      QBFC::Test::TxnFind.find_by_id(@sess, "123-456")
+    end
+  
+    it "should return a Transaction object" do
+      setup_request
+      list = QBFC::Test::TxnFind.find_by_id(@sess, "123-456")
+      list.should be_kind_of(QBFC::Test::TxnFind)
+    end
+  
+    it "should return nil if none found" do
+      setup_request
+      @response.should_receive(:GetAt).with(0).and_return(nil)
+      QBFC::Test::TxnFind.find_by_id(@sess, "123-456").should be_nil
+    end
   end
   
-  describe ".find_by_ref_or_id" do
-    it "should try to find_by_id"
-    it "should try to find_by_ref if id fails"
-    it "should return nil if both ref and id return nil"
+  describe ".find_by_name_or_id" do    
+    it "should try to find_by_id" do
+      QBFC::Test::TxnFind.should_receive(:find_by_id).with(@sess, "123-456").and_return("Txn By ID")
+      QBFC::Test::TxnFind.find_by_ref_or_id(@sess, "123-456").should == "Txn By ID"
+    end
+    
+    it "should try to find_by_ref if id fails" do
+      QBFC::Test::TxnFind.should_receive(:find_by_id).with(@sess, "123-456").and_return(nil)
+      QBFC::Test::TxnFind.should_receive(:find_by_ref).with(@sess, "123-456").and_return("Txn By Ref")
+      QBFC::Test::TxnFind.find_by_ref_or_id(@sess, "123-456").should == "Txn By Ref"
+    end
+    
+    it "should return nil if both ref and id return nil" do
+      QBFC::Test::TxnFind.should_receive(:find_by_id).with(@sess, "123-456").and_return(nil)
+      QBFC::Test::TxnFind.should_receive(:find_by_ref).with(@sess, "123-456").and_return(nil)
+      QBFC::Test::TxnFind.find_by_ref_or_id(@sess, "123-456").should be_nil
+    end
+
+    it "should be aliased as .find_by_unique_id" do
+      QBFC::Test::TxnFind.should_receive(:find_by_id).with(@sess, "123-456").and_return(nil)
+      QBFC::Test::TxnFind.should_receive(:find_by_ref).with(@sess, "123-456").and_return(nil)
+      QBFC::Test::TxnFind.find_by_unique_id(@sess, "123-456").should be_nil
+    end
   end
   
 end
