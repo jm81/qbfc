163db21f7337d5860de9eacff92f3d32cdec70ef
Jared Morgan <jmorgan@morgancreative.net>
2008-03-21 15:35:02 +0000

    Clean up lists/generated.rb (Includes bug fix: PayrollItemNonWage had been including Modifiable).

--- lib/qbfc/lists/generated.rb
+++ lib/qbfc/lists/generated.rb
@@ -2,14 +2,14 @@ module QBFC
 
   # Generated List types that will inherit directly from QBFC::List
   LIST_TYPES = %w{ BillingRate CustomerMsg CustomerType JobType  PaymentMethod
-      PayrollItemNonWage PayrollItemWage PriceLevel SalesRep SalesTaxCode ShipMethod ToDo
-      Vehicle VendorType}
+      PayrollItemNonWage PayrollItemWage PriceLevel SalesRep SalesTaxCode
+      ShipMethod ToDo Vehicle VendorType}
   
-  # Generated List types that do not accept Mod Requests
-  LIST_NO_MOD_TYPES = %w{ BillingRate CustomerMsg CustomerType JobType PaymentMethod
-      PayrollItemWage SalesTaxCode ShipMethod ToDo VendorType}
+  # Generated List types that do accept Mod Requests
+  # (most direct subclasses of List do not)
+  LIST_MOD_TYPES = %w{ PriceLevel SalesRep Vehicle }
   
   # Generate List subclasses
-  generate(LIST_TYPES, List, {Modifiable => LIST_TYPES - LIST_NO_MOD_TYPES})
+  generate(LIST_TYPES, List, {Modifiable => LIST_MOD_TYPES})
   
 end
