6dab0faadddc4966f87ec11a9eeebdd6dc127cf8
Jared Morgan <jmorgan@morgancreative.net>
2008-03-21 15:36:57 +0000

    Aesthetic clean up in transactions/generated.rb

--- lib/qbfc/transactions/generated.rb
+++ lib/qbfc/transactions/generated.rb
@@ -1,18 +1,25 @@
 module QBFC
 
   # Generated Transaction types
-  TXN_TYPES = %w{ARRefundCreditCard Bill BillPaymentCheck BillPaymentCreditCard BuildAssembly Charge Check CreditCardCharge
-      CreditCardCredit CreditMemo Deposit Estimate InventoryAdjustment Invoice ItemReceipt JournalEntry PurchaseOrder
-      ReceivePayment SalesOrder SalesReceipt SalesTaxPaymentCheck TimeTracking VehicleMileage VendorCredit}
+  TXN_TYPES = %w{ARRefundCreditCard Bill BillPaymentCheck BillPaymentCreditCard
+      BuildAssembly Charge Check CreditCardCharge CreditCardCredit CreditMemo
+      Deposit Estimate InventoryAdjustment Invoice ItemReceipt JournalEntry
+      PurchaseOrder ReceivePayment SalesOrder SalesReceipt SalesTaxPaymentCheck 
+      TimeTracking VehicleMileage VendorCredit}
 
   # Generated Transaction types that support TxnVoid Request
-  TXN_VOIDABLE_TYPES = %w{ARRefundCreditCard Bill BillPaymentCheck BillPaymentCreditCard Charge Check CreditCardCharge CreditCardCredit
-      CreditMemo Deposit InventoryAdjustment Invoice ItemReceipt JournalEntry SalesReceipt VendorCredit}
+  TXN_VOIDABLE_TYPES = %w{ARRefundCreditCard Bill BillPaymentCheck
+      BillPaymentCreditCard Charge Check CreditCardCharge CreditCardCredit
+      CreditMemo Deposit InventoryAdjustment Invoice ItemReceipt JournalEntry
+      SalesReceipt VendorCredit}
   
   # Generated Transaction types that don't support Mod Requests
-  TXN_NO_MOD_TYPES = %w{ARRefundCreditCard BillPaymentCreditCard Deposit InventoryAdjustment VehicleMileage VendorCredit }
+  TXN_NO_MOD_TYPES = %w{ARRefundCreditCard BillPaymentCreditCard Deposit
+    InventoryAdjustment VehicleMileage VendorCredit }
   
   # Generate Transaction subclasses
-  generate(TXN_TYPES, Transaction, {Modifiable => (TXN_TYPES - TXN_NO_MOD_TYPES), Voidable => TXN_VOIDABLE_TYPES})
+  generate(TXN_TYPES, Transaction,
+    { Modifiable => (TXN_TYPES - TXN_NO_MOD_TYPES),
+      Voidable => TXN_VOIDABLE_TYPES })
   
 end
